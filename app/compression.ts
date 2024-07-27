1dd75bd8a51e623a0512816653108b02c3dcf4f6
dontsudo <chotnt741@gmail.com>
2024-07-27 18:24:21 +0900

    handleCompression

--- app/compression.ts
+++ app/compression.ts
@@ -1,25 +1,27 @@
 import * as zlib from "node:zlib";
-import { promisify } from "node:util";
-
 import { HttpRequest, HttpResponse } from "./httpTypes";
-
-const gzipPromise = promisify(zlib.gzip);
 
 export const handleCompression = async (
   req: HttpRequest,
   res: HttpResponse
 ) => {
   const rawAcceptedEncodings = req.headers.get("Accept-Encoding");
-
-  // @example ["gzip", "deflate", "br"]
   const acceptedEncodings = rawAcceptedEncodings
     ? rawAcceptedEncodings.split(",").map((encoding) => encoding.trim())
     : [];
 
-  if (acceptedEncodings.includes("gzip")) {
+  console.log("acceptedEncodings", acceptedEncodings);
+  console.log("res.body", res.body);
+
+  if (acceptedEncodings.includes("gzip") && res.body) {
+    const compressed = await new Promise<Buffer>((resolve, reject) => {
+      zlib.gzip(res.body ?? "", (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      });
+    });
+
     res.headers.set("Content-Encoding", "gzip");
-    const body = Buffer.from(res.body ?? "");
-    const compressed = await gzipPromise(body);
     res.headers.set("Content-Length", `${compressed.length}`);
     res.body = compressed;
   }
